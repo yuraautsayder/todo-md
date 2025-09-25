d513f56d3f9079061cf407c8799b11e285605231
Yuriy Meresin <yuriymeresin@gmail.cok>
2025-09-25 20:12:47 +0300

    add todo

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>simple markdown todo</title>
+</head>
+<body>
+<?php
+// TODO add register and login with telegram
+// TODO add CRUD todo
+// TODO add todo template
+// TODO add readme and docs
+?>
+</body>
+</html>
